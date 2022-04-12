0a3277200e25cbf839e06d671a18a8964ad3e6c6
Sergii Dymchenko <sdym@fb.com>
2022-04-12 16:39:45 -0700

    WIP

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -3,14 +3,9 @@ set -ex -o pipefail
 
 nvidia-smi
 
-sudo apt-get -y install \
-  expect
-
 # install git lfs and checkout the blob files
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs=2.13.2
 git lfs install
 git lfs fetch
 git lfs checkout .
-
-sudo pkill -SIGHUP dockerd
